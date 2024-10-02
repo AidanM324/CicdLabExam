0a7c2d8162125618396ab25f1217eb45d99a64fb
ie.atu <G00423306@atu.ie>
2024-10-02 15:45:05 +0100

    Added delete mapping to productcontroller

--- src/main/java/ie/atu/week3lab/Product.java
+++ src/main/java/ie/atu/week3lab/Product.java
@@ -9,8 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 
 public class Product {
-    private String id;
     private String name;
     private String category;
+    private int id;
     private double price;
 }
--- src/main/java/ie/atu/week3lab/ProductController.java
+++ src/main/java/ie/atu/week3lab/ProductController.java
@@ -1,10 +1,7 @@
 package ie.atu.week3lab;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -14,8 +11,8 @@ public class ProductController {
     private List<Product> products = new ArrayList<>();
 
     public ProductController(){
-        products.add(new Product("Tv", "made by Sony", "Electronics", 101));
-        products.add(new Product("Radio", "made by Sony", "Electronics", 102));
+        products.add(new Product("Tv", "made by Sony", 101, 101));
+        products.add(new Product("Radio", "made by Sony", 102, 102));
 
     }
 
@@ -25,10 +22,28 @@ public class ProductController {
         return products;
     }
 
-    @PostMapping("addProduct")
+    @PostMapping("/addProduct")
     public ResponseEntity<List> addProduct(@RequestBody Product product)
     {
         products.add(product);
+        return ResponseEntity.ok(products);
+    }
+
+    @PutMapping("/{id}")
+    public String putId(@RequestParam int id, @RequestBody Product product)
+    {
+        return "product is being updated";
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteProduct(@PathVariable long id) {
+        int num = (int) id;
+
+        for (int count = 0; count < products.size(); count++) {
+            if (products.get(count).getId() == num) {
+                products.remove(count);
+            }
+        }
         return ResponseEntity.ok(products);
     }
 }
