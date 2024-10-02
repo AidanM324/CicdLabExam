aa5fa3f580d7d4d240a4f46d068635be1f73b599
ie.atu <G00423306@atu.ie>
2024-10-02 14:42:58 +0100

    made additions to product controller

--- src/main/java/ie/atu/week3lab/ProductController.java
+++ src/main/java/ie/atu/week3lab/ProductController.java
@@ -1,24 +1,28 @@
 package ie.atu.week3lab;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/products")
-
 public class ProductController {
     private List<Product> products = new ArrayList<>();
 
     public ProductController(){
-        products.add(new Product("Tv", "made by Sony", 899, 101));
-        products.add(new Product("Radio", "made by Sony", 99, 102));
+        products.add(new Product("Tv", "made by Sony", "Electronics", 101));
+        products.add(new Product("Radio", "made by Sony", "Electronics", 102));
 
     }
 
     @GetMapping("/getProducts")
-    public String getProducts()
+    public List<Product> getProducts()
     {
         return products;
+    }
+
+    @PostMapping("addProduct")
+    public ResponseEntity<List> addProduct(@RequestBody Product product)
+    {
+        products.add(product);
+        return ResponseEntity.ok(products);
     }
 }
