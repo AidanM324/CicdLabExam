a12b3437521ff582ec6dfe7968e4efa75ba272aa
ie.atu <G00423306@atu.ie>
2024-11-06 14:12:21 +0000

    Created new project IMS

---
+++ CicdLabExam/src/main/java/ie/atu/cicdlabexam/CicdLabExamApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.cicdlabexam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLabExamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLabExamApplication.class, args);
+    }
+
+}
